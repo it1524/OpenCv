dc44a7fcff408403f42b7568b524712eaf379f1a
Niki <niki.muhr@seznam.cz>
2018-11-04 19:43:54 +0100

    recognizer

--- camera.py
+++ camera.py
@@ -1,7 +1,15 @@
 import numpy as np
 import cv2
+import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {"person_name": 1}
+with open("labels.pickle", 'rb') as f:
+	old_labels = pickle.load(f)
+	labels = {v:k for k,v in old_labels.item()}
 
 #nastaveni kamery
 cap = cv2.VideoCapture(0)
@@ -13,10 +21,18 @@ while (True):
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 	for(x, y, w, h) in faces:
-		print(x,y,w,h)
 		roi_gray = gray[y:y+h, x:x+w]
 		roi_color = frame[y:y+h, x:x+w]
 
+		id_, conf = recognizer.predict(roi_gray)
+		if conf>= 45 and conf<= 85
+			print(id_)
+			print(labels[id_])
+			font = cv2.FONT_HERSHEY_SIMPLEX
+			name = labels[id_]
+			color = (255,255,255)
+			stroke = 2
+			cv2.putText(frame,name, (x,y), font,1,color,stroke,cv2,LINE_AA)
 
 		img_item = "my-image.png"
 		cv2.imwrite(img_item,roi_gray)
